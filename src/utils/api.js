d72200914d28baa26db48621e17329f3cb6d72c9
JishnuTP <tpjishnu5@gmail.com>
2025-01-24 11:59:03 +0530

    hdhc

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,5 @@
 
 
-export const API_BASE_URL = 'https://dnd-five-zeta.vercel.app/api'; // Adjust this based on your backend setup
+export const API_BASE_URL = 'https://dnd-drag-and-drop-mt.vercel.app/api'; // Adjust this based on your backend setup
 
 
